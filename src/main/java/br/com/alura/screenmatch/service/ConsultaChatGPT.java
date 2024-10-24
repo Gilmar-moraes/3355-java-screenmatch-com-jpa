c0cb3a563f8fb66d1160b0d3315788add8ccc72d
Gilmar Moraes <gilmar14moraes@hotmail.com>
2024-10-23 22:57:48 -0300

    Usando o Chat GPT para traduzir a sinopse

--- src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -5,7 +5,7 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
     public static String obterTraducao(String texto) {
-        OpenAiService service = new OpenAiService("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");
+        OpenAiService service = new OpenAiService("cole aqui sua chave da OpenAI");
 
         CompletionRequest requisicao = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
