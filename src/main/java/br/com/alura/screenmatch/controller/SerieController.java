da70612b882474cbe578b07779d17c6f77a04a52
Gilmar Moraes <gilmar14moraes@hotmail.com>
2025-01-30 11:31:00 -0300

    Mapeando a pángina inicial do screenmactch

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,33 +1,40 @@
 package br.com.alura.screenmatch.controller;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
-import br.com.alura.screenmatch.model.Serie;
-import br.com.alura.screenmatch.repository.SerieRepository;
+import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
     @Autowired
-    private SerieRepository serieRepository;
+    private SerieService serieService;
 
-    @GetMapping("/series")
+    @GetMapping()
     public List<SerieDTO> obterSeries() {
-        return serieRepository.findAll()
-                .stream()
-                .map(s -> new SerieDTO(s.getId(),
-                        s.getTitulo(),
-                        s.getTotalTemporadas(),
-                        s.getAvaliacao(),
-                        s.getGenero(),
-                        s.getAtores(),
-                        s.getPoster(),
-                        s.getSinopse()))
-                .collect(Collectors.toList());
+        return serieService.obterTodasAsSeries();
     }
+
+    @GetMapping("/top5")
+    public List<SerieDTO> obterTop5Series() {
+        return serieService.obterTop5Series();
+    }
+
+    @GetMapping("/lancamentos")
+    public List<SerieDTO> obterLacamentos() {
+        return serieService.obetrLancamentos();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obterPorId(@PathVariable Long id) {
+        return serieService.obterPorId(id);
+    }
+
 }
---
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -0,0 +1,60 @@
+package br.com.alura.screenmatch.service;
+
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+
+    @Autowired
+    private SerieRepository serieRepository;
+
+    public List<SerieDTO> obterTodasAsSeries() {
+        return this.converteDados(serieRepository.findAll());
+    }
+
+    public List<SerieDTO> obterTop5Series() {
+        return this.converteDados(serieRepository.findTop5ByOrderByAvaliacaoDesc());
+
+    }
+
+    public List<SerieDTO> obetrLancamentos() {
+        return this.converteDados(serieRepository.findTop5ByOrderByEpisodiosDataLancamentoDesc());
+    }
+
+    private List<SerieDTO> converteDados(List<Serie> series) {
+        return series.stream()
+                .map(s -> new SerieDTO(s.getId(),
+                        s.getTitulo(),
+                        s.getTotalTemporadas(),
+                        s.getAvaliacao(),
+                        s.getGenero(),
+                        s.getAtores(),
+                        s.getPoster(),
+                        s.getSinopse()))
+                .collect(Collectors.toList());
+    }
+
+    public SerieDTO obterPorId(Long id) {
+        Optional<Serie> serie = serieRepository.findById(id);
+        if (serie.isPresent()) {
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(),
+                    s.getTitulo(),
+                    s.getTotalTemporadas(),
+                    s.getAvaliacao(),
+                    s.getGenero(),
+                    s.getAtores(),
+                    s.getPoster(),
+                    s.getSinopse());
+        }
+        return null;
+    }
+}
